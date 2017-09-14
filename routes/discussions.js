0e7a170d63fe6a6e13fea3781e9857f3d16ed40c
Travis Rich <travis.s.rich@gmail.com>
2017-09-14 17:10:53 -0400

    Add post discussion route placeholder

--- routes/discussions.js
+++ routes/discussions.js
@@ -1,6 +1,11 @@
 import app from '../server';
 import { Label, Discussion, User } from '../models';
 
+app.post('/discussions', (req, res)=> {
+	console.log(req.body);
+	return res.status(201).json('I got it');
+});
+
 app.get('/discussions/:labelSlugs', (req, res)=> {
 	console.time('testDiscussionsQuery');
 	const slugs = req.params.labelSlugs.split('+');
